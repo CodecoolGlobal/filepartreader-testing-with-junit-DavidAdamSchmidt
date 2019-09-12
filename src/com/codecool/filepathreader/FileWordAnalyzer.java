7e45628e4833709daed20913c205c51c8f93f54e
David Schmidt <david.adam.schmidt@gmail.com>
2019-09-12 17:34:30 +0200

    add FileWordAnalyzer with constructor

---
+++ src/com/codecool/filepathreader/FileWordAnalyzer.java
@@ -0,0 +1,10 @@
+package com.codecool.filepathreader;
+
+public class FileWordAnalyzer {
+
+    private FilePathReader filePathReader;
+
+    public FileWordAnalyzer(FilePathReader filePathReader) {
+        this.filePathReader = filePathReader;
+    }
+}
